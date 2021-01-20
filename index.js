6890462d688565bd3a772869f3437645bec94fea
Joe Pennock <pennockjoe@gmail.com>
2021-01-20 15:23:34 -0800

    fixed start script to work with heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: 'https://js401-final-test-server.herokuapp.com/',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
     credentials: true
   }
